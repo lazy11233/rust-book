1e23ca88217b5d591c286411687820dd831f1ecb
liuzhao <liuzhao1@ynby.cn>
2026-04-26 11:28:16 +0800

    feat: ch05_struct

---
+++ ch05_struct/src/main.rs
@@ -0,0 +1,34 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    println!("{rect1:?}");
+    println!("长方体的面积是{}", rect1.area());
+    println!("rect1能容纳rect2吗？{}", rect1.can_hold(&rect2));
+}
